4cc2b9da69f96ae5d699f2f0d84e0b09c39be3c9
vky5 <vky05@proton.me>
2025-02-25 18:15:15 +0530

    added usermodel

---
+++ apps/backend/model/userModel.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: 8,
+    select: false,
+  },
+  img: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  //hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  // sometimes the new Token is generated way too soon before the timestamp for password change is created so we need to subtract some time from that timestamp
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } }); // this is the middleware that will run before every find query and it will pass an extra query in which active must be true or not equals false
+  next();
+});
+
+userSchema.methods.correctPassword = async function (password, actualPassword) {
+  return await bcrypt.compare(password, actualPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  const passwordChangedAt = this.passwordChangedAt;
+  if (passwordChangedAt) {
+    const changedTimeStamp = parseInt(passwordChangedAt.getTime() / 1000, 10);
+
+    return JWTTimestamp < changedTimeStamp;
+  }
+
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+const UserData = mongoose.model("UserData", userSchema);
+module.exports = UserData;
